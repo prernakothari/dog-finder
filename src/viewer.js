0fc646c75281911568cda92538b00937e7b157bd
Prerna Kothari <prernakotharius@gmail.com>
2021-01-20 15:21:49 -0800

    responsive bug solved

--- src/viewer.js
+++ src/viewer.js
@@ -16,7 +16,6 @@ class Viewer extends Component {
     this.state = {
       rowMap: Map(),
       desiredWidth: desiredWidth,
-      loadedImages: 0,
       maxNumberOfImagesInRow: Math.min(Math.floor(desiredWidth / 200), 5)
     }
   }
@@ -25,19 +24,26 @@ class Viewer extends Component {
     this.setState({ rowMap: Map() })
   }
 
-  resize = () => {
+  resize = (e) => {
+    e.preventDefault();
     let desiredWidth = window.innerWidth * 0.95
-    let rowMap = this.state.rowMap
-    rowMap.forEach((value, rowIndex) => {
-      rowMap = rowMap.updateIn([rowIndex, "indicesLoaded"], set => set.clear())
-      rowMap = rowMap.setIn([rowIndex, "width"], 0)
-      rowMap = rowMap.deleteIn([rowIndex, "finalHeight"])
-    })
+    let oldRowMap = this.state.rowMap
     let state = {
-      rowMap: rowMap,
+      rowMap: Map(),
       desiredWidth: desiredWidth,
-      maxNumberOfImagesInRow: Math.min(Math.floor(desiredWidth / 150), 4)
+      maxNumberOfImagesInRow: Math.min(Math.floor(desiredWidth / 200), 5)
     }
+    for (let i = 0; i < this.props.urlList.length; i++) {
+      let rowIndex = Math.floor(i / state.maxNumberOfImagesInRow);
+
+      let numberOfImagesInRow = state.maxNumberOfImagesInRow
+      if (rowIndex === Math.floor((this.props.urlList.length - 1) / state.maxNumberOfImagesInRow)) {
+        numberOfImagesInRow = this.props.urlList.length - state.maxNumberOfImagesInRow * rowIndex
+      }
+
+      state = this.addWidthToMap(document.getElementById(`image${i}`), rowIndex, i, oldRowMap.getIn([rowIndex, "height"]), numberOfImagesInRow, state)
+    }
+
     this.setState({ ...state })
   }
 
@@ -55,31 +61,31 @@ class Viewer extends Component {
   }
 
 
-  addWidthToMap = (imageElement, rowIndex, index, numberOfImagesInRow) => {
-    let rowMap = this.state.rowMap
+  addWidthToMap = (imageElement, rowIndex, index, height, numberOfImagesInRow, stateRef = undefined) => {
+    let state = stateRef ? stateRef : this.state
+    let rowMap = state.rowMap
     if (!rowMap.has(rowIndex)) {
-      rowMap = rowMap.set(rowIndex, { height: 30, width: 0 })
+      rowMap = rowMap.set(rowIndex, { width: 0 })
     }
-    let indicesLoaded = rowMap.getIn([rowIndex, "indicesLoaded"])
-    if (indicesLoaded === undefined) {
-      indicesLoaded = Set([])
-    }
-    let loadedImages = this.state.loadedImages
+    let indicesLoaded = rowMap.getIn([rowIndex, "indicesLoaded"], Set([]))
+
     if (!indicesLoaded.has(index)) {
       rowMap = rowMap.updateIn([rowIndex, "width"], w => w + imageElement.clientWidth)
       rowMap = rowMap.setIn([rowIndex, "indicesLoaded"], indicesLoaded.add(index))
 
-      if (loadedImages !== this.props.urlList.length) {
-        loadedImages++
-      }
-
       if (rowMap.getIn([rowIndex, "indicesLoaded"]).size === numberOfImagesInRow) {
-        let newHeight = rowMap.getIn([rowIndex, "height"]) * (this.state.desiredWidth / rowMap.getIn([rowIndex, "width"]))
-        console.log(`Height ${newHeight} for row : ${rowIndex}`);
-        rowMap = rowMap.setIn([rowIndex, "finalHeight"], newHeight)
+        let newHeight = imageElement.clientHeight / window.innerWidth * 100 * (state.desiredWidth / rowMap.getIn([rowIndex, "width"]))
+        rowMap = rowMap.setIn([rowIndex, "height"], newHeight)
       }
-      this.setState({ rowMap: rowMap, loadedImages: loadedImages })
+      if (stateRef === undefined)
+        this.setState({ rowMap: rowMap })
+      else
+        return { ...state, rowMap: rowMap }
     }
+  }
+
+  componentDidUpdate() {
+
   }
 
   render() {
@@ -95,13 +101,11 @@ class Viewer extends Component {
         numberOfImagesInRow = this.props.urlList.length - this.state.maxNumberOfImagesInRow * rowIndex
       }
 
-      if (this.state.rowMap.hasIn([rowIndex, "finalHeight"])) {
-        height = this.state.rowMap.getIn([rowIndex, "finalHeight"])
-      } else if (this.state.loadedImages === this.props.urlList.length) {
-        this.addWidthToMap(document.getElementById(`image${i}`), rowIndex, i, numberOfImagesInRow)
+      if (this.state.rowMap.hasIn([rowIndex, "height"])) {
+        height = this.state.rowMap.getIn([rowIndex, "height"])
       }
 
-      imageList.push(<img id={`image${i}`} src={this.props.urlList[i]} style={{ height: `${height}vw`, padding: "0.2%" }} onLoad={(e) => this.addWidthToMap(e.target, rowIndex, i, numberOfImagesInRow)}
+      imageList.push(<img id={`image${i}`} src={this.props.urlList[i]} style={{ height: `${height}vw`, padding: "0.2%" }} onLoad={(e) => this.addWidthToMap(e.target, rowIndex, i, height, numberOfImagesInRow)}
       />)
     }
 
